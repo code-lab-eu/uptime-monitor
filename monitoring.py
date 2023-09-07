4915be904eb7bc2537dad846b4d8fb29084c8a72
Veselka Marinova <vsl.marinova@gmail.com>
2023-09-07 19:12:21 +0300

    T926: Refactor and add speed test.

--- monitoring.py
+++ monitoring.py
@@ -2,18 +2,30 @@ import requests
 import os
 import ast
 
+from datetime import timedelta
 from dotenv import load_dotenv
+
+
+def notify_telegram(message):
+    api_token = os.getenv("API_TOKEN")
+    chat_id = os.getenv("CHAT_ID")
+    api_url = f'https://api.telegram.org/bot{api_token}/sendMessage'
+    requests.post(api_url, json={'chat_id': chat_id, 'text': message})
+
+
 def check_sites():
-    load_dotenv()
-    apiToken = os.getenv("API_TOKEN")
-    chatID = os.getenv("CHAT_ID")
-    apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
     for url in ast.literal_eval(os.getenv("URLS")):
         try:
-            website_url = requests.get(url, timeout=3)
+            website_url = requests.get(url, timeout=5)
             if website_url.status_code != 200:
-                requests.post(apiURL, json={'chat_id': chatID, 'text': f"%s is down" % url})
-        except Exception as e:
-            requests.post(apiURL, json={'chat_id': chatID, 'text': str(e)})
+                notify_telegram(f"%s is down" % url)
+            else:
+                if website_url.elapsed > timedelta(seconds=2):
+                    notify_telegram(f"%s is slow" % url)
 
+        except Exception as e:
+            notify_telegram(str(e))
+
+
+load_dotenv()
 check_sites()
