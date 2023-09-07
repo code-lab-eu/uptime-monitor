5f3b670d839764fd57ac5bfc9cd75c37f1cd6dd7
Veselka Marinova <vsl.marinova@gmail.com>
2023-09-07 18:45:47 +0300

    T926: Send notification when site is down.

--- monitoring.py
+++ monitoring.py
@@ -1,16 +1,19 @@
 import requests
 import os
+import ast
+
 from dotenv import load_dotenv
-def send_to_group_telegram(group_message):
+def check_sites():
     load_dotenv()
     apiToken = os.getenv("API_TOKEN")
     chatID = os.getenv("CHAT_ID")
     apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
+    for url in ast.literal_eval(os.getenv("URLS")):
+        try:
+            website_url = requests.get(url, timeout=3)
+            if website_url.status_code != 200:
+                requests.post(apiURL, json={'chat_id': chatID, 'text': f"%s is down" % url})
+        except Exception as e:
+            requests.post(apiURL, json={'chat_id': chatID, 'text': str(e)})
 
-    try:
-        response = requests.post(apiURL, json={'chat_id': chatID, 'text': group_message})
-        print(response.text)
-    except Exception as e:
-        print(
-
-send_to_group_telegram("Hello!")
+check_sites()
