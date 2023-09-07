6f751d341a5b647c89726f5f88eb834f728165d2
Veselka Marinova <vsl.marinova@gmail.com>
2023-09-07 19:36:48 +0300

    Make Timeout configurable.

--- monitoring.py
+++ monitoring.py
@@ -16,11 +16,11 @@ def notify_telegram(message):
 def check_sites():
     for url in ast.literal_eval(os.getenv("URLS")):
         try:
-            website_url = requests.get(url, timeout=5)
+            website_url = requests.get(url, timeout=30)
             if website_url.status_code != 200:
                 notify_telegram(f"%s is down" % url)
             else:
-                if website_url.elapsed > timedelta(seconds=2):
+                if website_url.elapsed > timedelta(seconds=int(os.getenv("TIMEOUT"))):
                     notify_telegram(f"%s is slow" % url)
 
         except Exception as e:
