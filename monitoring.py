8aaa3a9cae7484df8556e4eeaf07922f44189fbd
Veselka Marinova <vsl.marinova@gmail.com>
2023-09-27 22:46:13 +0300

    Check three times  before reporting.

--- monitoring.py
+++ monitoring.py
@@ -14,17 +14,26 @@ def notify_telegram(message):
 
 
 def check_sites():
+    # The number of times to check
+    max_attempts = 3
     for url in ast.literal_eval(os.getenv("URLS")):
-        try:
-            website_url = requests.get(url, timeout=30)
-            if website_url.status_code != 200:
-                notify_telegram(f"%s is down" % url)
-            else:
-                if website_url.elapsed > timedelta(seconds=int(os.getenv("TIMEOUT"))):
-                    notify_telegram(f"%s is slow" % url)
-
-        except Exception as e:
-            notify_telegram(str(e))
+        # Initialize a flag to track the type of issue (down or slow)
+        problems = []
+        for attempt in range(max_attempts):
+            try:
+                website_url = requests.get(url, timeout=10)
+                if website_url.status_code != 200:
+                    problems.append("down")
+                elif website_url.elapsed > timedelta(seconds=int(os.getenv("TIMEOUT"))):
+                    problems.append("slow")
+                else:
+                    problems = []
+                    break
+            except Exception as e:
+                problems.append(str(e))
+        if len(problems):
+            problems = ', '.join(problems)
+            notify_telegram(f"{url} is {problems}")
 
 
 load_dotenv()
